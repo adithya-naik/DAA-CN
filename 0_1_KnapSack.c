20e51e92e0cf35e011c46a7617b48c69080cd784
Jatoth Adithya Naik <jatothadithyanaik@gmail.com>
2025-03-02 23:52:26 +0530

    Update 0_1_KnapSack.c

--- 0_1_KnapSack.c
+++ 0_1_KnapSack.c
@@ -6,6 +6,7 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
+
 // Returns the maximum value that can be
 // put in a knapsack of capacity W
 int knapSack(int W, int wt[], int val[], int n)
